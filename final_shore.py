5c7f1cff007f5b141f3953e9d5eea781b2844d1c
amanpandey104 <amanpandey1504@gmail.com>
2020-01-20 01:20:16 +0530

    code from station to raspberry

---
+++ final_shore.py
@@ -0,0 +1,113 @@
+import time
+import pygame
+import socket
+import threading
+import pickle
+import sys
+import cv2
+import numpy as np
+import struct 
+import zlib
+
+server_address = ('192.168.1.168', 5001)
+
+pygame.init()
+j = pygame.joystick.Joystick(0)
+j.init()
+print ('Initialized Joystick : %s' % j.get_name())
+
+"""
+1.Left analog x axis
+2.Left analog y axis
+3.LT +ve RT -ve
+4.Right y axis
+5.Right x axis
+6.A
+7.B
+8.X
+9.Y
+10.LB
+11.RB
+12.BACK
+13.START
+14.LEFT CLICK
+15.RIGHT CLICK
+"""
+
+def get():
+    out = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    it = 0 #iterator
+    pygame.event.pump()
+    #Read input from the two joysticks       
+    for i in range(0, j.get_numaxes()):
+        out[it] = round(j.get_axis(i),1)
+        it+=1
+    #Read input from buttons
+    for i in range(0, j.get_numbuttons()):
+        out[it] = j.get_button(i)
+        it+=1
+    if(abs(out[1])>abs(out[0])):
+        out[0]=0.0
+    else:
+        out[1]=0.0
+    if(out[1]==0.1 or out[1]==-0.1):
+        out[1]=0.0
+    if(out[0]==0.1 or out[0]==-0.1):
+        out[0]=0.0
+    if(out[3]==0.1 or out[3]==-0.1):
+        out[3]=0.0
+    if(out[4]==0.1 or out[4]==-0.1):
+        out[4]=0.0
+    out[1]=out[1]*10
+    out[0]=out[0]*10
+    out[3]=out[3]*10
+    out[4]=out[4]*10
+    s=str(out).strip('[]')
+    data = pickle.dumps(s)
+    return data
+
+def recv():
+    sock2=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock2.connect(('192.168.1.167',5002))
+    while True:
+        sock2.send(get())
+        time.sleep(.01)
+
+def send():
+    sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock1.bind(server_address)
+    sock1.listen(5)
+    conn,addr = sock1.accept()
+    data = b""
+    payload_size = struct.calcsize(">L")
+    print("payload_size: {}".format(payload_size))
+    while True:
+        while len(data) < payload_size:
+            print("Recv: {}".format(len(data)))
+            data += conn.recv(4096)
+
+        print("Done Recv: {}".format(len(data)))
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack(">L", packed_msg_size)[0]
+        print("msg_size: {}".format(msg_size))
+        while len(data) < msg_size:
+            data += conn.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+
+        frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        h,w = frame.shape[:2]
+
+        frame1=cv2.resize(frame,(2*w,2*h), interpolation = cv2.INTER_LINEAR)   
+        
+        cv2.imshow('ImageWindow',frame1)
+        cv2.waitKey(1)
+
+sending = threading.Thread(target=send)
+receiving = threading.Thread(target=recv)
+sending.start()
+receiving.start()
+sending.join()
+receiving.join()
