630b9d0e7ae3bd437bea9539bf5a297afe37e072
masquerrade <60103730+masquerrade@users.noreply.github.com>
2020-01-20 21:21:53 +0530

    Update extractInteger.ino

--- extractInteger.ino
+++ extractInteger.ino
@@ -1,3 +1,5 @@
+
+//Code to extract integers from a srtring seperated by "," and whitespaces and store them as seperate elements of an array 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -8,9 +10,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   char *ptr;
   int i1;
-  int i2;
 
-  char yourstring[]=" 155, 12345, 456, 765,4.56";
+  char yourstring[]="155, 12345, 456, 765, 489";
   int num[10];
   int i=0;
   num[i]=atoi(yourstring);
@@ -24,7 +25,10 @@ void loop() {
     num[i]=i1;
     ptr=strchr(ptr,',');
   }
-  Serial.println(num[4]);
+  for(int j=0;j<=i;j++)
+  {
+  Serial.println(num[j]);
+  }
   delay(500);
   }
   
