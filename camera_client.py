07c448a955ed742790bd5243bd0d4c10e0f64823
amanpandey104 <amanpandey1504@gmail.com>
2020-01-20 22:09:51 +0530

    Transferring live feed via TCP socket using Python

---
+++ camera_client.py
@@ -0,0 +1,43 @@
+import socket
+import sys
+import cv2
+import pickle
+import numpy as np
+import struct 
+import zlib
+
+HOST='192.168.1.168'
+PORT=5001
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((HOST,PORT))
+s.listen(5)
+
+conn,addr=s.accept()
+
+data = b""
+payload_size = struct.calcsize(">L")
+print("payload_size: {}".format(payload_size))
+while True:
+    while len(data) < payload_size:
+        print("Recv: {}".format(len(data)))
+        data += conn.recv(4096)
+
+    print("Done Recv: {}".format(len(data)))
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
+    print("msg_size: {}".format(msg_size))
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    h,w = frame.shape[:2]
+
+    frame1=cv2.resize(frame,(2*w,2*h), interpolation = cv2.INTER_LINEAR)   
+    
+    cv2.imshow('ImageWindow',frame1)
+    cv2.waitKey(1)
