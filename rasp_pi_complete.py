e5e87d35c5f1bc759f88110ec023fbbd45b4e5d7
amanpandey104 <amanpandey1504@gmail.com>
2020-01-21 05:14:52 +0530

    complete raspberry interface

--- rasp_pi_complete.py
+++ rasp_pi_complete.py
@@ -10,7 +10,7 @@ import serial
 ser = serial.Serial('COM7',9600)
 msg = ""
 
-server_address = ('192.168.1.167',5002)
+server_address = ('192.168.43.167',5002)
 
 cam = cv2.VideoCapture(0)
 
@@ -31,26 +31,10 @@ def recv():
         except:
             continue
         ser.write(msg.encode())
-        k = ser.readline().decode('utf-8')
-        k1 = ser.readline().decode('utf-8')
-        k2 = ser.readline().decode('utf-8')
-        k3 = ser.readline().decode('utf-8')
-        k4 = ser.readline().decode('utf-8')
-        k5 = ser.readline().decode('utf-8')
-        k6 = ser.readline().decode('utf-8')
-        k7 = ser.readline().decode('utf-8')
-        k8 = ser.readline().decode('utf-8')
-        k9 = ser.readline().decode('utf-8')
-        k10 = ser.readline().decode('utf-8')
-        k11 = ser.readline().decode('utf-8')
-        k12 = ser.readline().decode('utf-8')
-        print(k,k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12)
-        #print("Received:",msg)
-        #ardToPi(msg)
 
 def send():
     sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock2.connect(('192.168.1.168',5001))
+    sock2.connect(('192.168.43.168',5001))
     connection = sock2.makefile('wb')
     while True:
         ret, frame = cam.read()
